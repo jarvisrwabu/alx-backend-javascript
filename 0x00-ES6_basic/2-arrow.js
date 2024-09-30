812cd07a505ad045a77ff38b7669f39478464358
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-09-30 16:09:33 +0300

    Change function to react arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+const getNeighborhoodList = () => {
+  this.santhis.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+};
+
+export default getNeighborhoodList;
