9829c1e367470c310e76e9c5dd5a5d6d6fa91d72
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-08 23:39:27 +0300

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+const getListStudents = () => {
+  const myObject = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return myObject;
+};
