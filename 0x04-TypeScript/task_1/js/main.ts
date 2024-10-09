6114a06a08e16977f741c0f93554522dcfe91bfc
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-09 19:20:41 +0300

    Add function and its interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,14 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 };
+
+// Interface defining the structure of the printTeacher function
+interface PrintTeacherFunction {
+    (fName: string, lName: string): string; // Parameters and return type
+}
+
+const printTeacher : PrintTeacherFunction = (fName: string, lName: string) => {
+    const t1 = fName.charAt(0);
+    const t2 = lName;  
+    return t1.concat(".", t2);
+};
