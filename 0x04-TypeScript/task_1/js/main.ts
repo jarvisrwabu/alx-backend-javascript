ac4035fb02ab617f6652ff3a37c68382eaa088fe
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-09 21:29:25 +0300

    More Typescript - Class and constructor interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -22,3 +22,38 @@ const printTeacher : PrintTeacherFunction = (fName: string, lName: string) => {
     const t2 = lName;  
     return t1.concat(".", t2);
 };
+
+
+// Interface for the constructor
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+
+// Interface for the class
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(x: string, y: string){
+        this.firstName = x;
+        this.lastName = y; 
+    }
+
+    workOnHomework(): string {
+        const myString = "Currently working";
+        return myString;
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+};
