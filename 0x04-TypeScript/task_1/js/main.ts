a4ca2393ada3a9657c6710eb33a4f7be519c67bd
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-09 17:08:15 +0300

    Teacher interface - with optional attributes and additional attributes

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;  // This is an optional attribute
+    location: string;
+    [key: string]: any;          // Allows additional attributes with any name and type
+
+};
